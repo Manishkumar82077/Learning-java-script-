0cf3cc11e3da861bc869b0123f98193782f2d217
Manish kumar <91789078+Manishkumar82077@users.noreply.github.com>
2023-12-29 09:56:51 +0000

    learn about variable

---
+++ basic/_01_variable.js
@@ -0,0 +1,9 @@
+const accountId=1144553;
+let accountEmail="manishk7543@gmail.com";
+var accountpassword="123456";
+accountCity="jaipur";
+accountid=2;
+accountEmail="none";
+console.log("manish kumar");
+console.log(accountId);
+console.table([accountEmail,accountId,accountpassword])
