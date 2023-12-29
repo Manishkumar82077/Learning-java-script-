6d733d9ef716d111b8151fd22274c50fca7c2978
Manish kumar <91789078+Manishkumar82077@users.noreply.github.com>
2023-12-29 18:52:16 +0000

    dataType conversion

---
+++ basic/_03_dataType_conversion.js
@@ -0,0 +1,9 @@
+let score =33
+console.log(typeof score)
+let StringScore=String(score)
+console.log(typeof StringScore)
+
+let age = "33"
+console.log(typeof age)
+let NuberAge=Number(age)
+console.log(typeof NuberAge)
