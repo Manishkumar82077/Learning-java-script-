9719af1b3509272e8d3653da65ae8b9fa8238018
Manish kumar <91789078+Manishkumar82077@users.noreply.github.com>
2023-12-29 18:27:31 +0000

    learn-about Data type

---
+++ basic/_02_dataType.js
@@ -0,0 +1,11 @@
+// alert(3-2);// run in browser not in node js
+let name= "manish"
+let age = 13
+let isLegal=true
+let youAcc;
+let youPass=null
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLegal)
+console.log(typeof youAcc);
+
