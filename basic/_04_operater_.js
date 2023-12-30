fbafbacadfdff81b45b2d01429dec54ec5039825
Manish kumar <91789078+Manishkumar82077@users.noreply.github.com>
2023-12-30 12:32:06 +0000

    learn about operater

---
+++ basic/_04_operater_.js
@@ -0,0 +1,20 @@
+let a = 9
+let b = 5
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b)
+
+
+// #comparision of data type
+console.log(1 + 2 + "2")//=>32
+console.log("3" + "3")//=>33
+
+console.log(null > 0) //==> false
+console.log(null == 0)//=> false
+console.log(null >= 0) //==> ture
+
+
---
+++ basic/_05_operation.js
@@ -0,0 +1,15 @@
+let s1="manish"
+let s2 = "kumar"
+let s3 = s1+s2
+console.log(s3)
+
+const hero=["msnish","kumar","drr"]
+console.log(hero[1])
+let obj = {
+    name :"hr",
+    age:16
+}
+const n1=function myname(s1) {
+    console.log(s1);
+}
+console.log(n1);
