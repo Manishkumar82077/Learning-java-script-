f72d08bea4dd2ee04bdcf9a5d98e4d018de397eb
Manish kumar <91789078+Manishkumar82077@users.noreply.github.com>
2023-12-30 13:19:14 +0000

    leran about Nums & Math

---
+++ basic/_07_Nums_Math.js
@@ -0,0 +1,10 @@
+const score =300;
+console.log(score)
+const balance = new Number(123)
+console.log(balance)
+console.log(balance.toFixed(2));
+console.log(balance.toPrecision(3));//fouce on decimal before 
+const hundred = 10000000000;
+console.log(hundred.toLocaleString(`en-IN`));
+
+console.log(Math.random()) //range (0-1)
