2a8b283b6eca251a42eb0589d6d5b0d8e43768d9
Manish kumar <91789078+Manishkumar82077@users.noreply.github.com>
2023-12-30 13:18:21 +0000

    learn about string

---
+++ basic/_06_string.js
@@ -0,0 +1,20 @@
+const s1 = "manish"
+const repoCount=30
+console.log(`hello my name is ${s1} and my repocount is ${repoCount}`)
+
+const repoName=new String("codeJsFile");
+console.log(repoName);
+console.log(typeof s1);
+console.log(typeof repoName);
+
+// inbuild function string
+
+
+console.log(repoName.length)
+console.log(repoName.toLowerCase)
+console.log(repoName.charAt(4))
+console.log(repoName.indexOf("F"))
+
+
+
+
