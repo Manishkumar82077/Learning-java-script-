7c64a3d39cb86100388a81b1a14a72a3404893a7
Manish kumar <91789078+Manishkumar82077@users.noreply.github.com>
2024-01-02 18:50:52 +0000

    date time

--- Basic_01/_07_Nums_Math.js
+++ Basic_01/_07_Nums_Math.js
@@ -8,3 +8,11 @@ const hundred = 10000000000;
 console.log(hundred.toLocaleString(`en-IN`));
 
 console.log(Math.random()) //range (0-1)
+
+// console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.4));
+console.log(Math.floor(4.6))
+console.log(Math.min(4,3,5,6,8,9,32,2,4,5,6,7,4));
+console.log(Math.max(4,3,5,6,8,9,32,2,4,5,6,7,4));
---
+++ Basic_01/_08_Date_time.js
@@ -0,0 +1,16 @@
+let MyDate = new Date()
+console.log(MyDate.toString());
+console.log(MyDate.toDateString());
+console.log(MyDate.toLocaleString());
+
+// let CurentDate = new Date(2023,0,14)
+let CurentDate = new Date("2023-12-14")
+
+console.log(CurentDate);
+
+
+let MytimeStamp = Date.now()
+console.log(MytimeStamp);
+console.log(CurentDate.getTime());
+console.log(Date.now());
+
